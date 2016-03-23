fb10f0ac55bec309bdab9fbe1950acad636d01a6
matoola <matej@spletnimolj.si>
2016-03-23 10:51:30 +0100

    first

---
+++ footer.php
@@ -0,0 +1,20 @@
+<footer class="content-info page_footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3 col-xs-12">
+                <?php dynamic_sidebar('sidebar-footer1'); ?>
+            </div>
+            <div class="col-sm-6 col-xs-12">
+                <?php dynamic_sidebar('sidebar-footer2'); ?>
+            </div>
+            <div class="col-sm-3 col-xs-12 text-right">
+                <?php dynamic_sidebar('sidebar-footer3'); ?>
+            </div>
+        </div>
+        <div class="row copyright">
+            <div class="col-xs-12 text-right">
+                &copy; 2016 Maklu
+            </div>
+        </div>
+    </div>
+</footer>
